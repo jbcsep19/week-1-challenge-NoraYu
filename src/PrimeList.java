639253f9863d2d71a948b9da9ae5097967ffd328
NoraYu <seeyounora@gmail.com>
2019-09-06 12:05:04 -0400

    add list

--- src/PrimeList.java
+++ src/PrimeList.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class PrimeList {
-}
+    public static void main(String[] args){
+        System.out.println("Please enter a number, I will help you check if all the numbers less and equal to it are prime number");
+        Scanner numInput=new Scanner(System.in);
+        int num=numInput.nextInt();
+        Boolean prime_check=true;
+        if(num<=0){prime_check=false;
+            System.out.println(num+" is not a prime number"); }
+        for(int j=1; j<num+1; j++){
+            if(j>2){
+                for(int i=2;i<j;i++){
+                  if(j%i==0){
+                       prime_check=false;
+                       System.out.println(j+" is not a prime number ");
+                       break;
+                        }
+                  else{
+                        prime_check=true;
+
+                        }}
+                }
+            else if(j==2){System.out.println(j+" is a prime number");}
+            else{
+                prime_check=false;
+                System.out.println(j+" is not a prime number"); }
+         if(prime_check==true){
+              System.out.println(j+" is a prime number");}
+    }}}
--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class PrimeNumbers {
-}
+    public static void main(String[] args){
+        String go_on="Y";
+        while(go_on.equalsIgnoreCase("Y")){
+            System.out.println("Please enter a number, I will help you check if it's a prime number");
+            Scanner numInput=new Scanner(System.in);
+            int num=numInput.nextInt();
+            Boolean prime_check=true;
+            if(num>2){
+                for(int i=2;i<num;i++){
+                    if(num%i==0){
+                        prime_check=false;
+                        System.out.println(num+" is not a prime number");
+                        break;
+                    }
+                    else{
+                        prime_check=true;
+                    }}}
+            else if(num==2){prime_check=true;}
+
+            else{
+                prime_check=false;
+                System.out.println(num+" is not a prime number"); }
+            if(prime_check==true){
+                System.out.println(num+" is a prime number");}
+            System.out.println("Would you like to input another number? (Y/N)");
+            go_on=numInput.next();
+
+
+        } }}
+
